14abeaed8e870bdac496cff66919112b8cf3101b
gmw-zjw <mingwei2222@126.com>
2019-09-18 06:50:25 +0800

    +order page preCacheImage function

--- lib/page/order/page/order_info_page.dart
+++ lib/page/order/page/order_info_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_derp/util/image_util.dart';
+import 'package:provider/provider.dart';
 
 class OrderInfoPage extends StatefulWidget {
   @override
@@ -6,6 +8,28 @@ class OrderInfoPage extends StatefulWidget {
 }
 
 class _OrderInfoPageState extends State<OrderInfoPage> {
+
+  OrderPageProvider provider = OrderPageProvider()  
+
+  @override
+  void initState() { 
+    super.initState();
+    // widget binding instance
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _preCacheImage();
+    });
+  }
+
+  // 预先加载image
+  _preCacheImage() {
+    precacheImage(ImageUtils.getAssetImage("order/xdd_n"), context);
+    precacheImage(ImageUtils.getAssetImage("order/dps_s"), context);
+    precacheImage(ImageUtils.getAssetImage("order/dwc_s"), context);
+    precacheImage(ImageUtils.getAssetImage("order/ywc_s"), context);
+    precacheImage(ImageUtils.getAssetImage("order/yqx_s"), context);
+  } 
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
