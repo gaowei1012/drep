c67d2a1e88224df606645724f8b8194b60ce7d1e
gmw-zjw <mingwei2222@126.com>
2019-09-18 06:59:17 +0800

    - order info page content

--- lib/page/order/page/order_info_page.dart
+++ lib/page/order/page/order_info_page.dart
@@ -9,27 +9,6 @@ class OrderInfoPage extends StatefulWidget {
 
 class _OrderInfoPageState extends State<OrderInfoPage> {
 
-  //OrderPageProvider provider = OrderPageProvider()  
-
-  @override
-  void initState() { 
-    super.initState();
-    // widget binding instance
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _preCacheImage();
-    });
-  }
-
-  // 预先加载image
-  _preCacheImage() {
-    precacheImage(ImageUtils.getAssetImage("order/xdd_n"), context);
-    precacheImage(ImageUtils.getAssetImage("order/dps_s"), context);
-    precacheImage(ImageUtils.getAssetImage("order/dwc_s"), context);
-    precacheImage(ImageUtils.getAssetImage("order/ywc_s"), context);
-    precacheImage(ImageUtils.getAssetImage("order/yqx_s"), context);
-  } 
-
-
   @override
   Widget build(BuildContext context) {
     return Container(
--- lib/page/order/page/order_page.dart
+++ lib/page/order/page/order_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_derp/util/image_util.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_derp/page/order/provider/order_page_provider.dart';
 
 class OrderPage extends StatefulWidget {
   @override
@@ -6,6 +9,26 @@ class OrderPage extends StatefulWidget {
 }
 
 class _OrderPageState extends State<OrderPage> {
+
+  OrderPageProvider provider = OrderPageProvider();
+
+  @override
+  void initState() { 
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // 预加载图片
+      _preCacheImage();
+    });
+  }
+
+  _preCacheImage() {
+    precacheImage(ImageUtils.getAssetImage("order/xdd_n"), context);
+    precacheImage(ImageUtils.getAssetImage("order/dps_s"), context);
+    precacheImage(ImageUtils.getAssetImage("order/dwc_s"), context);
+    precacheImage(ImageUtils.getAssetImage("order/ywc_s"), context);
+    precacheImage(ImageUtils.getAssetImage("order/yqx_s"), context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
