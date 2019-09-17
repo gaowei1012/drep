ae689917de7ffedfc169d083a8beaef7c2fb0781
gmw-zjw <mingwei2222@126.com>
2019-09-18 06:27:29 +0800

    + order info page

---
+++ lib/page/order/page/order_info_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrderInfoPage extends StatefulWidget {
+  @override
+  _OrderInfoPageState createState() => _OrderInfoPageState();
+}
+
+class _OrderInfoPageState extends State<OrderInfoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('order info'),
+    );
+  }
+}
