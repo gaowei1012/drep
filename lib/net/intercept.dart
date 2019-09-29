2287b88e48472af10b0409798412bd2798c9dd10
gmw-zjw <mingwei2222@126.com>
2019-09-29 16:33:23 +0800

    + net

---
+++ lib/net/intercept.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:sprintf/sprintf.dart';
+
+import 'dio_utils.dart';
+import 'error_handle.dart';
+
+class AuthInterceptor extends Interceptor {
+  @override
+  onRequest(RequestOptions options) {
+  }
+}
