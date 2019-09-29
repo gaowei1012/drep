74c0c75ed507d9fc5db77b7530c2f8388746adb2
gmw-zjw <mingwei2222@126.com>
2019-09-29 16:25:32 +0800

    + http api static

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/Sky/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/Sky/development/flutter_derp"
-export "FLUTTER_TARGET=/Users/Sky/development/flutter_derp/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/Sky/Desktop/drep"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/Sky/development/flutter/bin/cache/artifacts/engine/ios"
