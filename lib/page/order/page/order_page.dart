6f8c1b03471e26bfd36fc214c8d6fb6348363f70
gmw-zjw <mingwei2222@126.com>
2019-10-01 20:27:10 +0800

    fix order page

--- lib/page/order/page/order_page.dart
+++ lib/page/order/page/order_page.dart
@@ -117,6 +117,51 @@ List<Widget> _sliverBuilder(BuildContext context) {
           title: const Text("订单"),
         )
       ),
-    )
+    ),
+    // SliverPersistentHeader(
+    //   pinned: true,
+    //   delegate: SliverAppBarDelegate(
+    //     DecoratedBox(
+    //       decoration: BoxDecoration(
+    //         image: DecorationImage(
+    //           image: ImageUtils.getAssetImage("order/order_bg1"),
+    //           fit: BoxFit.fill
+    //         )
+    //       ),
+          // child: Padding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          //   child: Container(
+          //     child: Center(
+          //       child: Text("this is my card"),
+          //     ),
+          //   ),
+          // ),
+        // )
+        // , 80.0
+      // ),
+    // )
   ];
+}
+
+class SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final Widget widget;
+  final double height;
+  SliverAppBarDelegate(this.widget, this.height);
+
+  // minHeight 和 maxHeight 的值设置为相同时，header就不会收缩了
+  @override
+  double get minExtent => height;
+
+  @override
+  double get maxExtent => height;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return widget;
+  }
+
+  @override
+  bool shouldRebuild(SliverAppBarDelegate oldDelegate) {
+    return true;
+  }
 }
