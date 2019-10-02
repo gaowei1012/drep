63ddac0270cea78ccd21e0b8ce1cfe4d426d5813
gmw-zjw <mingwei2222@126.com>
2019-10-02 21:58:26 +0800

    add state layout widget

--- lib/widgets/state_layout.dart
+++ lib/widgets/state_layout.dart
@@ -1,11 +1,17 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-class StateLayout extends StatelessWidget {
+class StateLayout extends StatefulWidget {
+  @override
+  _StateLayoutState createState() => _StateLayoutState();
+}
+
+class _StateLayoutState extends State<StateLayout> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("state layout"),
+      child: Text('this is state layout page'),
     );
   }
 }
