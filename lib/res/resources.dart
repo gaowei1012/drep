dfe03cbe5102806e9fc251a43019a7bc2e508010
gmw-zjw <mingwei2222@126.com>
2019-10-03 17:12:41 +0800

    +recommend

---
+++ lib/res/resources.dart
@@ -0,0 +1,13 @@
+
+export 'colors.dart';
+export 'gaps.dart';
+export 'dimens.dart';
+export 'styles.dart';
+
+import 'package:flutter_derp/widgets/load_inage.dart';
+import 'package:flutter/widgets.dart';
+
+
+class Images {
+  static const Widget arrowRight = const LoadAssetImage("ic_arrow_right", height: 16.0, width: 16.0,);  
+}
---
+++ lib/res/styles.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'colors.dart';
+import 'dimens.dart';
+
+class TextStyles {
+  static const TextStyle textMain12 = const TextStyle(
+    fontSize: Dimens.font_sp12,
+    color: Colours.app_main,
+  );
+  static const TextStyle textMain14 = const TextStyle(
+    fontSize: Dimens.font_sp14,
+    color: Colours.app_main,
+  );
+  static const TextStyle textNormal12 = const TextStyle(
+    fontSize: Dimens.font_sp12,
+    color: Colours.text_normal,
+  );
+  static const TextStyle textSize12 = const TextStyle(
+    fontSize: Dimens.font_sp12,
+  );
+  static const TextStyle textSize16 = const TextStyle(
+    fontSize: Dimens.font_sp16,
+  );
+  static const TextStyle textBold14 = const TextStyle(
+      fontSize: Dimens.font_sp14,
+      fontWeight: FontWeight.bold
+  );
+  static const TextStyle textBold16 = const TextStyle(
+      fontSize: Dimens.font_sp16,
+      fontWeight: FontWeight.bold
+  );
+  static const TextStyle textBold18 = const TextStyle(
+    fontSize: Dimens.font_sp18,
+    fontWeight: FontWeight.bold
+  );
+  static const TextStyle textBold24 = const TextStyle(
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold
+  );
+  static const TextStyle textBold26 = const TextStyle(
+      fontSize: 26.0,
+      fontWeight: FontWeight.bold
+  );
+  static const TextStyle textGray10 = const TextStyle(
+    fontSize: Dimens.font_sp10,
+    color: Colours.text_gray,
+  );
+  static const TextStyle textGray12 = const TextStyle(
+    fontSize: Dimens.font_sp12,
+    color: Colours.text_gray,
+  );
+  static const TextStyle textGray14 = const TextStyle(
+    fontSize: Dimens.font_sp14,
+    color: Colours.text_gray,
+  );
+  static const TextStyle textGray16 = const TextStyle(
+    fontSize: Dimens.font_sp16,
+    color: Colours.text_gray,
+  );
+  static const TextStyle textGrayC12 = const TextStyle(
+    fontSize: Dimens.font_sp12,
+    color: Colours.text_gray_c,
+  );
+  static const TextStyle textGrayC14 = const TextStyle(
+    fontSize: Dimens.font_sp14,
+    color: Colours.text_gray_c,
+  );
+
+  static const TextStyle textWhite14 = const TextStyle(
+    fontSize: Dimens.font_sp14,
+    color: Colors.white,
+  );
+
+  static const TextStyle textWhite12 = const TextStyle(
+    fontSize: Dimens.font_sp12,
+    color: Colors.white,
+  );
+
+  static const TextStyle textDark12 = const TextStyle(
+    fontSize: Dimens.font_sp12,
+    color: Colours.text_dark,
+  );
+
+  static const TextStyle textDark14 = const TextStyle(
+    fontSize: Dimens.font_sp14,
+    color: Colours.text_dark,
+  );
+}
